778de20e944d9cc82f38402e895eb6a2b3365290
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2025-01-23 23:15:47 +0530

    Experiment 1a

---
+++ Experiment-1-a/Test.java
@@ -0,0 +1,12 @@
+//This experiment is to show the writing, compiling and executuing a java program
+
+public class Test{
+    public static void main(String args[]){
+        System.out.println("Kunal_11_CSEA");
+    }
+}
+
+
+// In the Terminal change the directory to the .java file location then:
+// >> javac Test.java
+// >> java Test
